cc729904bab203724b32a98550dc4987dca9a2f1
Tusharborgaonkar <105264709+Tusharborgaonkar@users.noreply.github.com>
2022-08-17 10:51:47 +0530

    Add files via upload

---
+++ Event.js
@@ -0,0 +1,26 @@
+function demOnclick(){
+    document.body.style.backgroundcolor="lightcoral"
+    alert("on click");
+}
+
+function demoonclick(){
+    alert ("on Double click");
+}
+
+function demoonDblclick(){
+    document.getElementById("txtFocus").style.backgroundcolor="Red";
+}
+
+function demoonblur(){
+    document.getElementById("txtBlur").style.backgroundColor="yellow";
+}
+
+function demokeypress(){
+    alert("keypress Done");
+}
+
+function demoonkeyup(){
+    alert("keypress Done");
+}
+
+
